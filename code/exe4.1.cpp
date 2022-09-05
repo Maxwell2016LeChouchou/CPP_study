dbe39a308b0e7512e1b5b7b6b6c696966031a076
Maxwell <wangjz625@163.com>
2022-09-05 15:32:13 +0800

    exe4.1

--- code/exe4.1.cpp
+++ code/exe4.1.cpp
@@ -9,33 +9,31 @@ using namespace std;
 
 // 分别用全局函数和成员函数判断两个立方体是否相等。
 
+
+
 class Cube
 {
 
-public:
-    
-    void set_mH(float h)
+    Cube(float h, float w, float l)
     {
         m_H = h;
-    } 
+        m_W = w;
+        m_L = l;     
+    }
+
+public:
+
     float get_mH()
     {
         return m_H;
     }
 
-    void set_mW(float w)
-    {
-        m_W = w;
-    }
+
     float get_mW()
     {
         return m_W;
     }
 
-    void set_mL(float l)
-    {
-        m_L = l;
-    }
     float get_mL()
     {
         return m_L;
@@ -48,23 +46,22 @@ private:
 
 };
 
+bool same(Cube c1, Cube c)
+{
+    if (c1.get_mH() == c.get_mH() && c1.get_mL() == c.get_mL() && c1.get_mW() == c.get_mW())
+    {
+        return true; 
+    }
+
+    else return false;
+
+}
+
 int main()
 {
-    Cube c;
-    cout << "Please input length of the cube: " << endl;
-    float l;
-    cin >> l;
-    c.set_mL(l);
+    Cube c(1,2,3);
 
-    cout << "Please input width of the cube: " << endl;
-    float w;
-    cin >> w;
-    c.set_mW(l);
-    
-    cout  << "Please input height of the cube: " << endl;
-    float h;
-    cin >> h;
-    c.set_mH(h);
+    vector<vector<float>> arr_l; 
     
     // find area of the cube
     float area =  2*c.get_mH()*c.get_mL() + 2*c.get_mH()*c.get_mW() +  2*c.get_mW()*c.get_mL();
@@ -72,10 +69,39 @@ int main()
     cout << "The area of the cube: " << area << endl; 
 
     //find volume of the cube
-    float volume =  c.get_mH()*c.get_mL() *c.get_mW();
+    float volume = c.get_mH()*c.get_mL() *c.get_mW();
 
     cout << "The volume of the cube: " << volume << endl;
 
+    Cube c1;
+    cout << "Please input length of the cube: " << endl;
+    float l1;
+    cin >> arr_l[0][] // 这里可以用for循环存数组;
+    c1.set_mL(arr_l(0));
+    c.set_mL(arr_l(1));
+
+    cout << "Please input width of the cube: " << endl;
+    float w1;
+    cin >> w1;
+    c1.set_mW(w1);
+    
+    cout  << "Please input height of the cube: " << endl;
+    float h1;
+    cin >> h1;
+    c1.set_mH(h1);
+    
+    // find area of the cube
+    float area1 =  2*c1.get_mH()*c1.get_mL() + 2*c1.get_mH()*c1.get_mW() +  2*c1.get_mW()*c1.get_mL();
+
+    cout << "The area of the cube1: " << area1 << endl; 
+
+    //find volume of the cube
+    float volume1 = c1.get_mH()*c1.get_mL() *c1.get_mW();
+
+    cout << "The volume of the cube1: " << volume1 << endl;
+
+    same(c, c1);
+    cout << same(c, c1) << endl;
 
     return 0;
 
